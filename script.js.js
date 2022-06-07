30253fc8aa8836a49ce0f92e161b99df7be3e160
Gurudeep <gurudeepnaik1999@gmail.com>
2022-06-07 14:14:42 +0530

    mywebpage

---
+++ script.js.js
@@ -0,0 +1,22 @@
+const dish1= document.getElementById("dish1");
+const dish2= document.getElementById("dish2");
+const dish3= document.getElementById("dish3");
+const dish4= document.getElementById("dish4");
+const dish5= document.getElementById("dish5");
+const dish6= document.getElementById("dish6");
+
+dish1.addEventListener('click',()=>{
+  dish6.src= "./images/d1.png"
+})
+dish2.addEventListener('click',()=>{
+  dish6.src= "./images/d2.png"
+})
+dish3.addEventListener('click',()=>{
+  dish6.src= "./images/d3.png"
+})
+dish4.addEventListener('click',()=>{
+  dish6.src= "./images/d4.png"
+})
+dish5.addEventListener('click',()=>{
+  dish6.src= "./images/d5.png"
+})
